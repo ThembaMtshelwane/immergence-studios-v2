7e5633ba95a8d629242d831f0dd7c3b080354048
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-23 14:30:37 +0200

    Team layout responsive

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,12 +1,12 @@
 const Team = () => {
   return (
-    <div className="bg-red-400 min-h-screen ">
-      <h2 className="my-8 text-center text-4xl sm:text-6xl md:text-8xl font-extrabold">
+    <div className="bg-red-400 min-h-screen py-10 ">
+      <h2 className="my-10 text-center text-4xl sm:text-6xl md:text-8xl font-extrabold">
         MEET THE TEAM
       </h2>
       <div className="grid grid-cols-5 grid-rows-2">
-        <div className="col-[1/4] bg-red-700 h-100 w-96 border justify-self-end self-center"></div>
-        <div className="col-[3/4] row-[2/4] bg-red-950 h-100 w-96 border"></div>
+        <div className="col-[2/4] bg-red-700 h-100 max-w-96  border"></div>
+        <div className="col-[3/5]  row-[2/3] bg-red-950 h-100 max-w-96 border"></div>
       </div>
     </div>
   );
