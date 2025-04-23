f1afb27544f72dca5ddb14513c5881b6d10263f7
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-23 11:57:41 +0200

    Imaged added

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,13 @@
 const Image = () => {
-  return <div>Image</div>;
+  return (
+    <div className="h-screen border">
+      <img
+        className="h-full w-full object-center object-cover"
+        src="https://images.unsplash.com/photo-1745106673075-d616222217b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Image;
