4695b922bf54b20447c0f44e3b4603cf898c46c5
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-23 11:55:41 +0200

    Servive list item resposnive

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,14 +1,16 @@
+import Service from "./ui/Service";
+
 const Services = () => {
   return (
-    <div className="bg-blue-600 h-screen py-4">
+    <div className="bg-blue-600 min-h-screen py-4">
       <h2 className="text-6xl text-center p-2 my-4 font-extrabold sm:text-start sm:text-8xl sm:my-0">
         SERVICES
       </h2>
-      <div className="border h-full flex flex-col gap-4">
-        <div className="bg-blue-400 h-[20%]"></div>
-        <div className="bg-blue-400 h-[20%]"></div>
-        <div className="bg-blue-400 h-[20%]"></div>
-        <div className="bg-blue-400 h-[20%]"></div>
+      <div className="border flex flex-col gap-4">
+        <Service />
+        <Service />
+        <Service />
+        <Service />
       </div>
     </div>
   );
---
+++ src/components/ui/Service.tsx
@@ -0,0 +1,20 @@
+const Service = () => {
+  return (
+    <div className="bg-blue-400 grid grid-rows-[40%_20%_40%] h-[250px] sm:grid-rows-1 sm:grid-cols-3">
+      <div className="col-span-full self-center sm:col-span-1 sm:px-4 md:justify-self-center">
+        Name of service
+      </div>
+      <p className="col-span-full sm:col-[2/3] self-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero enim
+        provident suscipit maiores molestias temporibus.
+      </p>
+      <img
+        className="h-full w-full object-center object-cover row-[1/2] col-span-full sm:col-[3/3]"
+        src="https://images.unsplash.com/photo-1745106673075-d616222217b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Service;
