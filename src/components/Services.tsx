83dcff6acdcf63f426340884d4bf42ec798f5b68
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-23 11:10:52 +0200

    Services layout

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,5 +1,17 @@
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="bg-blue-600 h-screen py-4">
+      <h2 className="text-6xl text-center p-2 my-4 font-extrabold sm:text-start sm:text-8xl sm:my-0">
+        SERVICES
+      </h2>
+      <div className="border h-full flex flex-col gap-4">
+        <div className="bg-blue-400 h-[20%]"></div>
+        <div className="bg-blue-400 h-[20%]"></div>
+        <div className="bg-blue-400 h-[20%]"></div>
+        <div className="bg-blue-400 h-[20%]"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Services;
