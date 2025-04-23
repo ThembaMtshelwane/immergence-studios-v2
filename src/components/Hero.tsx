9d2ab1e96583e0ad2c40192f865a5ef476fabaf3
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-23 10:35:51 +0200

    Responsive Hero layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,12 @@
 const Hero = () => {
   return (
-    <div className="border border-blue-500 h-screen grid grid-rows-[10%_40%_50%]">
+    <div className="border border-blue-500 h-screen grid grid-rows-[10%_40%_50%] md:grid-rows-[40%_60%] md:grid-cols-2">
       <div className="border bg-black text-white">
         <div>LOGO</div>
         <div>Hamburger</div>
       </div>
       <div className="bg-red-400">IMMERGENCE STUDIOS</div>
-      <div className="border bg-blue-400">Video</div>
+      <div className="border bg-blue-500 col-span-full">Video</div>
     </div>
   );
 };
