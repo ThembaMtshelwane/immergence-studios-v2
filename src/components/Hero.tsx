c1ddfd4eb2917cb8734346b9670c3ba9bb2b998e
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-24 16:35:24 +0200

    Navbar added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,9 +3,19 @@ import InfiiteCarousel from "./ui/InfiiteCarousel";
 const Hero = () => {
   return (
     <div className="border border-blue-500 h-screen grid grid-rows-[10%_40%_50%] md:grid-rows-[40%_60%] md:grid-cols-2">
-      <div className="border bg-black text-white">
-        <div>LOGO</div>
-        <div>Hamburger</div>
+      <div className="border flex relative">
+        <img
+          className="h-full w-[30%] md:w-full object-cover object-center bg-amber-500 "
+          src="logo.svg"
+          alt=""
+        />
+        <nav className="hidden sm:flex justify-around sm:absolute w-[70%] sm:right-0 border md:left-0 text-2xl md:w-full md:fixed z-50 bg-red-200 ">
+          <div>Home</div>
+          <div>About</div>
+          <div>Services</div>
+          <div>Team</div>
+          <div>Contact</div>
+        </nav>
       </div>
       <div className="bg-red-400 text-7xl sm:text-9xl md:text-7xl lg:text-9xl flex overflow-hidden items-center justify-center">
         <InfiiteCarousel />
