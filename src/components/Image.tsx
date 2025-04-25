d6e4e71ed1f5d411111d3ab6a963f17fc8c922a1
ThembaMtshelwane <thembamm3@gmail.com>
2025-04-25 15:02:40 +0200

    Image - Image height doubled to 200vh

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,6 +1,6 @@
 const Image = () => {
   return (
-    <div className="h-screen border relative z-50">
+    <div className="h-[200vh] border relative z-50">
       <img
         className="h-full w-full object-center object-cover"
         src="https://images.unsplash.com/photo-1745106673075-d616222217b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
